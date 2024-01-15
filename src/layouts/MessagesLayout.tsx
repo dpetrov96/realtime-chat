895b29cb5669e2301c2e7707fededfcc93066499
Dimitar Petrov <dimiturpetrov96@gmail.com>
2024-01-15 12:29:40 +0200

    Fix layout issues

--- src/layouts/MessagesLayout.tsx
+++ src/layouts/MessagesLayout.tsx
@@ -9,9 +9,6 @@ const Header = dynamic(() => import("@/components/Header"), {
   ssr: false,
 });
 
-const HEADER_HEIGHT = 58;
-const SIDEBAR_WIDTH = 300;
-
 const MessagesLayout = ({ children }: { children: React.ReactNode }) => {
   const params = useParams();
   const groupId = `${params?.groupId}`;
@@ -19,11 +16,11 @@ const MessagesLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
       <Header />
-      <div style={{ display: "flex", width: "100%", height: `calc(100vh - ${HEADER_HEIGHT}px)`, marginTop: HEADER_HEIGHT }}>
-        <div style={{ width: SIDEBAR_WIDTH, height: "100%", borderRight: "1px solid rgb(229, 231, 235)" }}>
+      <div className="messages-layout">
+        <div className="left-sidebar">
           <GroupsList selectedGroupId={groupId} />
         </div>
-        <div style={{ width: `calc(100% - ${SIDEBAR_WIDTH}px)`, height: "100%" }}>
+        <div className="content">
           {children}
         </div>
       </div>
